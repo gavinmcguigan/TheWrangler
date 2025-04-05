50195e5cbdf5c643288550aabf000a3f07091243
Gav <gavin.mcguigan1979@gmail.com>
2025-04-05 18:12:19 +0200

    Add preview to fzf command

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ fn execute_ag_command() -> Vec<u8> {
 fn execute_fzf_command(ag_out: Vec<u8>) -> String {
     // Create fzf command and set stdin and stdout to be piped
     let mut fzf = Command::new("fzf");
+    fzf.arg("--preview").arg("sudo -E batcat --style=full --color=always {}").arg("--preview-window").arg("top:80%:wrap");
     fzf.stdin(Stdio::piped());
     fzf.stdout(Stdio::piped());
 
